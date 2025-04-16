8395a2292e4140fea5a358b40d098af7688d5874
Jonas Pleyer <jonas.dev@pleyer.org>
2025-04-17 01:02:45 +0200

    restructure plotting script; match and name output files for identical results for rules

--- ca-rules/main.py
+++ ca-rules/main.py
@@ -55,7 +55,7 @@ def generate_rule_name(rule):
     return "".join([str(b) for b in binary]), f"{decimal:03}"
 
 
-def plot_rule(rule, start: np.ndarray, folder: Path):
+def compute_rule(rule, start: np.ndarray, folder: Path):
     (n_grid_x,) = start.shape
     n_grid_y = n_grid_x
     total = np.zeros((n_grid_y, n_grid_x), dtype=int)
@@ -72,16 +72,36 @@ def plot_rule(rule, start: np.ndarray, folder: Path):
     x_low = np.min(np.where(np.any(total > 0, axis=0)))
     x_high = np.max(np.where(np.any(total > 0, axis=0)))
     total = total[0:n, x_low - 2 : x_high + 2]
-    # total = total[0:n]
+    return total
 
-    binary_name, decimal_name = generate_rule_name(rule)
+
+def save_rule(total, rules, folder: Path):
+    names = [generate_rule_name(r)[1] for r in rules]
+    name = "-".join(names)
     if np.sum(total[1:]) > 0 and np.any(total[1] != total[0]):
         folder.mkdir(parents=True, exist_ok=True)
         img = Image.fromarray(255 * total.astype(np.uint8))
-        img.save(folder / f"rule-{decimal_name}.png", dpi=(300, 300))
-    else:
-        pass
-        # print(binary_name, decimal_name)
+        img.save(folder / f"rule-{name}.png", dpi=(300, 300))
+
+
+def plot_rules(
+    rules,
+    start,
+    folder,
+    n_rules: int | None,
+):
+    imgs_rules = []
+    for rule in tqdm(rules, total=n_rules):
+        img = compute_rule(rule, start, folder)
+        for i, saved_rules in imgs_rules:
+            if i.shape == img.shape and np.all(i == img):
+                saved_rules.append(rule)
+                break
+        else:
+            imgs_rules.append((img, [rule]))
+
+    for img, saved_rules in imgs_rules:
+        save_rule(img, saved_rules, folder)
 
 
 if __name__ == "__main__":
@@ -90,8 +110,7 @@ if __name__ == "__main__":
     start = np.zeros(n_grid_x, dtype=int)
     start[5 * 45] = 1
 
-    for rule in tqdm(ca_rule_generator(), total=32):
-        plot_rule(rule, start, folder=Path("single1"))
+    plot_rules(ca_rule_generator(), start, Path("single1"), 32)
 
     # Plot Double1 Histories
     n_grid_x = 450
@@ -99,8 +118,7 @@ if __name__ == "__main__":
     start[5 * 30] = 1
     start[5 * 60] = 1
 
-    for rule in tqdm(ca_rule_generator(), total=32):
-        plot_rule(rule, start, folder=Path("double1-spaced"))
+    plot_rules(ca_rule_generator(), start, Path("double1-spaced"), 32)
 
     # Plot Double1 Histories
     n_grid_x = 450
@@ -108,13 +126,11 @@ if __name__ == "__main__":
     start[5 * 45 - 1] = 1
     start[5 * 45 + 1] = 1
 
-    for rule in tqdm(ca_rule_generator(), total=32):
-        plot_rule(rule, start, folder=Path("double1-close"))
+    plot_rules(ca_rule_generator(), start, Path("double1-close"), 32)
 
     # Plot alternating
     n_gridx = 450
     start = np.zeros(n_grid_x, dtype=int)
     start[5 * 30 : 5 * 60][::2] = 1
 
-    for rule in tqdm(ca_rule_generator(), total=32):
-        plot_rule(rule, start, folder=Path("alternating"))
+    plot_rules(ca_rule_generator(), start, Path("alternating"), 32)
