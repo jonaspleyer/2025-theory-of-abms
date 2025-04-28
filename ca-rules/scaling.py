f1123a29c2e407a76ed9e5c3e23fbcf8f22e8317
Jonas Pleyer <jonas.dev@pleyer.org>
2025-04-28 18:08:42 +0200

    rework script; change colors; add von Neumann in 3D

--- ca-rules/scaling.py
+++ ca-rules/scaling.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+COLOR1 = "#e8702a"
+COLOR2 = "#ffbe4f"
+COLOR3 = "#6bd2db"
+COLOR4 = "#0ea7b5"
+COLOR5 = "#0c457d"
+
 
 def moore_1d(n):
     return 2 * n + 1
@@ -28,7 +34,7 @@ def neumann_dDim(n, d):
     if d == 2:
         return neumann_2d(n)
     else:
-        return None
+        return 1 + 2 * np.sum([neumann_dDim(k, d - 1) for k in np.arange(1, n + 1)])
 
 
 def neumann_dDim_kStep(n, d, k):
@@ -40,30 +46,36 @@ def exponential(n):
 
 
 if __name__ == "__main__":
+    plt.rcParams.update(
+        {
+            "font.family": "Courier New",  # monospace font
+            "font.size": 14,
+            "axes.titlesize": 14,
+            "axes.labelsize": 14,
+            "xtick.labelsize": 14,
+            "ytick.labelsize": 14,
+            "legend.fontsize": 14,
+            "figure.titlesize": 14,
+        }
+    )
+
     fig, ax = plt.subplots(figsize=(8, 8))
 
     x = np.arange(20)
 
-    ax.plot(x, moore_1d(x), label="$1D$", c="k")
-    # ax.plot(x, moore_dDim_kStep(x, 1, 2), label="$1D$ $2$-step", c="darkmagenta")
-    for i in range(2, 4):
-        ax.plot(
-            x,
-            moore_dDim(x, i),
-            label=f"${i}D$ Moore",
-            c="k",
-            linestyle=(0, (1, 10 - 3 * i)),
-        )
-    for i in range(2, 3):
-        ax.plot(
-            x,
-            neumann_dDim(x, i),
-            label=f"${i}D$ Neumann",
-            c="k",
-            linestyle=(0, (5, 10 - 3 * i)),
-        )
-
-    ax.plot(x, exponential(x), label="Exponential $2^n$", c="coral")
+    linestyles = ["-", "-", "--", ":", "-."]
+    ax.plot(x, moore_1d(x), label="1D", c=COLOR1, linestyle=":")
+    ax.plot(x, exponential(x), label="Exponential $2^n$", c=COLOR4)
+    ax.plot(x, moore_2d(x), label="2D Moore", c=COLOR1, linestyle="--")
+    ax.plot(x, neumann_2d(x), label="2D von Neumann", c=COLOR5, linestyle="--")
+    ax.plot(x, moore_dDim(x, 3), label="3D Moore", c=COLOR1, linestyle="-")
+    ax.plot(
+        x,
+        [neumann_dDim(xi, 3) for xi in x],
+        label="3D von Neumann",
+        c=COLOR5,
+        linestyle="-",
+    )
 
     ax.set_yscale("log")
     ax.set_xlabel("Iterations")
@@ -72,8 +84,8 @@ if __name__ == "__main__":
 
     ax.legend(
         loc="upper center",
-        bbox_to_anchor=(0.5, 1.10),
-        ncol=5,
+        bbox_to_anchor=(0.5, 1.12),
+        ncol=3,
         frameon=False,
     )
     ax.grid(True, which="major", linestyle="-", linewidth=0.75, alpha=0.25)
